ca277e4a7797d3d206dd52e8657b80129caa17b4
keiliving <keiliving6@gmail.com>
2021-04-05 22:18:08 +0900

    first

---
+++ WarikanKun/WarikanKunApp.swift
@@ -0,0 +1,17 @@
+//
+//  WarikanKunApp.swift
+//  WarikanKun
+//
+//  Created by keiliving on 2021/04/05.
+//
+
+import SwiftUI
+
+@main
+struct WarikanKunApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
